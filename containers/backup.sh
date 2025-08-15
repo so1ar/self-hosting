772b7206d9e62d9f510e25f91d66d3200a71f606
so1ar <so1ar114514@gmail.com>
2025-08-15 20:55:56 +0800

    Update backup script

--- containers/backup.sh
+++ containers/backup.sh
@@ -1,35 +1,68 @@
 #!/bin/bash
 
-# 备份文件夹，按需修改
-BACKUP_FOLDER=/home/user/backup
-# 容器文件夹，按需修改
-CONTAINER_FOLDER=/home/user/containers/freshrss
+#### 开始配置
 
-# 进入 FreshRSS 文件夹
-cd ${BACKUP_FOLDER}/freshrss
+# 自建 Gotify 的 url
+GOTIFY_URL=https://gotify.example.com
+# Gotify 的 token
+GOTIFY_TOKEN=SuperSecretToken
+
+# FreshRSS 的项目路径
+FRESHRSS_DIR=/home/user/containers/freshrss
+# 备份路径
+BACKUP_DIR=/home/user/backup
+
+#### 结束配置
+
+# 创建临时文件夹
+mkdir /tmp/backup
+cd /tmp/backup
 
 # 显示备份时间
-rm -rf *.txt
-time=$(date +%Y%m%d%H%M)
-echo ${time} > ${time}.txt
+time=$(date +%Y-%m-%d-%H%M)
 
 # 停止容器
-docker stop freshrss
-
-# 旧的备份文件
-mv FreshRSS-backup.tgz.gpg FreshRSS-backup-old.tgz.gpg
+docker stop freshrss >> /dev/null
 
 # 开始备份 FreshRSS
-tar -czf FreshRSS-backup.tgz -C ${CONTAINER_FOLDER} .
+tar -czf FreshRSS-backup-${time}.tgz -C ${FRESHRSS_DIR} .
 
 # 加密文件，需要改成自己的公钥
-gpg --recipient so1ar --encrypt FreshRSS-backup.tgz
+gpg --recipient so1ar --encrypt FreshRSS-backup-${time}.tgz
 
-# 删除未加密文件
-rm -rf FreshRSS-backup.tgz
+# 将加密后的文件移动到备份文件夹
+mv FreshRSS-backup-${time}.tgz.gpg ${BACKUP_DIR}/freshrss/
 
-# 上传文件到坚果云
-rclone sync ${BACKUP_FOLDER} nutstore:backup
+# 删除早于十天的文件
+find ${BACKUP_DIR}/freshrss/ -mtime +5 -name "*.*" -exec rm -rf {} \;
 
 # 重新启动容器
-docker start freshrss
+docker start freshrss >> /dev/null
+
+# 上传文件
+export ALIYUNPAN_CONFIG_DIR=/home/user/.config/aliyunpan
+/usr/local/bin/aliyunpan sync start -ldir ${BACKUP_DIR} -pdir /backup --drive resource --mode upload --policy exclusive --cycle onetime --up 1 --dp 1 --log true > /tmp/backup/aliyunpan-${time}.log
+
+# 推送消息
+UPLOAD_STATUS=$(grep '已完成' /tmp/backup/aliyunpan-${time}.log)
+if [ -z "${UPLOAD_STATUS}" ]; then
+    FULL_LOG=$(cat /tmp/backup/aliyunpan-${time}.log)
+    /usr/local/bin/gotify push --url ${GOTIFY_URL} --token ${GOTIFY_TOKEN} --priority 10 "备份同步失败 \n${FULL_LOG}"
+else
+    DELETED=$(grep '多余文件' /tmp/backup/aliyunpan-${time}.log)
+    UPLOADED=$(grep '上传文件' /tmp/backup/aliyunpan-${time}.log)
+    if [ -z "${DELETED}" ]; then
+        DELETED=没有文件删除
+    fi
+
+    if [ -z "${UPLOADED}" ]; then
+        UPLOADED=没有文件上传
+    fi
+
+    /usr/local/bin/gotify push --url ${GOTIFY_URL} --token ${GOTIFY_TOKEN} --priority 4 "${DELETED} \n${UPLOADED}"
+fi
+
+# 删除临时文件
+rm -rf /tmp/backup
+
+# vim: ts=4 sts=4 sw=4 et
